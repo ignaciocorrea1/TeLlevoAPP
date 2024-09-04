964e57dbe8505dbd231e7c7d1a804e5843f13389
unknown <cetecom@AVAWSV01LC0301.duoc.acad>
2024-09-04 12:57:54 -0400

    recuperación(validación 1, cambio estetico)

--- src/app/recuperar/recuperar.page.ts
+++ src/app/recuperar/recuperar.page.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { NavigationExtras, Router } from '@angular/router';
 
 @Component({
   selector: 'app-recuperar',
@@ -7,8 +8,38 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RecuperarPage implements OnInit {
 
-  constructor() { }
+  constructor(private router: Router) {}
 
+  user = {
+    username: "",
+  };
+
+  mensaje = "";
+
+  validar() {
+    if (this.user.username.length > 2 && this.user.username.length < 9) {
+      //if (this.user.password.length == 4) {
+        //Funciona
+      this.mensaje = '';
+        /* console.log(this.user.username);
+        let navigationExtras: NavigationExtras = {
+          state: {
+            username: this.user.username,
+            password: this.user.password,
+          },
+        };
+        this.router.navigate(['/inicio'], navigationExtras); */
+      /* } else {
+        console.log('Contraseña incorrecta');
+        this.mensaje = 'Contraseña incorrecta';
+        //No funciona
+      } */
+    } else {
+      console.log('Usuario no cumple (min: 3, max: 8)');
+      this.mensaje = 'Usuario no cumple (min: 3, max: 8)';
+      //Tampoco funciona
+    }
+  }
   ngOnInit() {
   }
 
